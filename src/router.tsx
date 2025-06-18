777d9de1b499d3d8d05beece052de9c2848a8981
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-18 16:42:40 +0530

    implemnet-component-test-for-loginpage

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import Categorries from "./pages/Categorries";
+import LoginPage from "./pages/login/login";
 
 export const router = createBrowserRouter([
   {
@@ -8,7 +8,7 @@ export const router = createBrowserRouter([
     element: <HomePage />,
   },
   {
-    path: "/categories",
-    element: <Categorries />,
+    path: "/login",
+    element: <LoginPage />,
   },
 ]);
