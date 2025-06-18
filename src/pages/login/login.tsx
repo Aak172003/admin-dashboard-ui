7f82fa3c981069b06713c3f70ea00b18f9cd8acb
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-19 00:23:30 +0530

    mock-matchmedia

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -4,14 +4,6 @@ import { LockOutlined, UserOutlined } from "@ant-design/icons";
 const LoginPage = () => {
   return (
     <>
-      {/* <h2>Sign in</h2>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
-      <button>Log in</button>
-      <label htmlFor="remember-me">Remember me</label>
-      <input type="checkbox" id="remember-me" />
-      <a href="#">Forgot password?</a> */}
-
       <Layout
         style={{
           height: "100vh",
