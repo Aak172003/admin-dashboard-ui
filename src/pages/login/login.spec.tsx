1e32b381b1c9e9b4fd31d5c4f7dcd9aa95888a9f
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-19 00:15:18 +0530

    implemnet-component-test-for-loginpage

--- src/pages/login/login.spec.tsx
+++ src/pages/login/login.spec.tsx
@@ -11,7 +11,9 @@ describe("Login Page", () => {
     expect(screen.getByPlaceholderText("Password")).toBeInTheDocument();
 
     expect(screen.getByRole("button", { name: "Log in" })).toBeInTheDocument();
-    expect(screen.getByRole("checkbox", { name: "Remember me" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("checkbox", { name: "Remember me" })
+    ).toBeInTheDocument();
 
     expect(screen.getByText("Forgot password?")).toBeInTheDocument();
   });
