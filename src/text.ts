1dcd18f25c831340c551efcaade55d7ee8c99fca
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-22 00:01:54 +0530

    add-auto-refresh-token-via-axios-interceptors

--- src/text.ts
+++ src/text.ts
@@ -1,3 +1,4 @@
 export const ENDPOINT_LOGIN = "/auth/login";
 export const ENDPOINT_SELF = "/auth/self";
 export const ENDPOINT_LOGOUT = "/auth/logout";
+export const ENDPOINT_REFRESH_TOKEN = "/auth/refresh";
