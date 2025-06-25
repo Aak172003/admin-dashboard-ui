6416974a30ebd79a9099bf0f4ee3a331b328a4d9
Aakashmoondive502 <aakash.prajapati@moondive.co>
2025-06-25 11:05:01 +0530

    create filter card

--- src/pages/users/userFilter.tsx
+++ src/pages/users/userFilter.tsx
@@ -1,9 +1,50 @@
-import { Card } from "antd";
+import { Button, Card, Col, Input, Row, Select } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
 
 const UserFilter = () => {
   return (
     <Card>
-      <h1>User Filter</h1>
+      <Row justify="space-between">
+        <Col span={16}>
+          <Row gutter={20}>
+            <Col span={8}>
+              <Input.Search placeholder="Search" />
+            </Col>
+            <Col span={8}>
+              <Select
+                style={{ width: "100%" }}
+                allowClear={true}
+                placeholder="Select Role"
+              >
+                <Select.Option value="admin">Admin</Select.Option>
+                <Select.Option value="manager">Manager</Select.Option>
+                <Select.Option value="customer">Customer</Select.Option>
+              </Select>
+            </Col>
+            <Col span={8}>
+              <Select
+                style={{ width: "100%" }}
+                allowClear={true}
+                placeholder="Select Status"
+              >
+                <Select.Option value="banned">Banned</Select.Option>
+                <Select.Option value="active">Active</Select.Option>
+              </Select>
+            </Col>
+          </Row>
+        </Col>
+        <Col
+          span={8}
+          style={{
+            display: "flex",
+            justifyContent: "end",
+          }}
+        >
+          <Button type="primary" icon={<PlusOutlined />}>
+            Add User
+          </Button>
+        </Col>
+      </Row>
     </Card>
   );
 };
